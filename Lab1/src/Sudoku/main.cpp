c8e0ea8db86369510fa12b649f5037de336c6f7f
FengYangyang <fengyangyang@hnu.edu.cn>
2020-02-27 14:18:22 +0800

    basic version

---
+++ Lab1/src/Sudoku/main.cpp
@@ -0,0 +1,8 @@
+
+
+
+int main(){
+    // build a process/thread to manage the input
+
+    // build a process/thread to manage the problem solving
+}
---
+++ Lab1/src/Sudoku/sudoku_solver.h
@@ -0,0 +1,14 @@
+#include "global.h"
+#include <string>
+
+string resultBuffer[RESULT_BUFFER_LEN];
+
+// the program to solve one part of the problem
+void SudokuSolverHelper(string);
+
+
+// the program to solve the single problem
+void SudokuSolver();
+
+
+void SudokuResult();
