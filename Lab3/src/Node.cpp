14def50885e0bb746078c22ffc4f2ff5aa49b900
syy <shiyuany1@outlook.com>
2020-05-21 15:53:03 +0800

    impore Node

--- Lab3/src/Node.cpp
+++ Lab3/src/Node.cpp
@@ -1,19 +1,57 @@
 #include "Node.hpp"
 
-#include <arpa/inet.h>
-#include <cstring>
-#include <ctype.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <thread>
-#include <unistd.h>
+std::mutex m;
+std::condition_variable empty;
+
+const int BUF_SIZE = 512000;
+bool flag = 0; // 阻塞
+volatile unsigned long long global_e_id = 0;
 
 namespace kv_2pc_sys {
-void Coor::get_command() { return; }
-bool Coor::parse_command() { return true; }
+// Start here
+void Coor::work_main() {
+  std::thread client_listener(server,nullptr);
+  std::thread PC_communicate(FSM,nullptr);
+  client_listener.detach();
+  PC_communicate.detach();
+  return ;
+}
 
-void Coor::init() {
+// Server
+void Coor::get_command(const int client_sockfd,
+                       const struct sockaddr_in &client_addr) {
+  char ipbuf[128];
+  char buf[BUF_SIZE];
+  memset(buf, 0, BUF_SIZE);
+
+  while (1) {
+    // read data, 阻塞读取
+    int len = recv(client_sockfd, buf, sizeof(buf), flag);
+    if (len == -1) {
+      close(client_sockfd);
+      // close(server_sockfd);
+      perror("read error");
+    } else if (len == 0) { // 这里以len为0表示当前处理请求的客户端断开连接
+      break;
+    }
+    Event tmp_e;
+    {
+      std::lock_guard<std::mutex> lockGuard(m);
+      global_e_id++;
+      tmp_e.e_id = global_e_id;
+      tmp_e.command = std::string(buf);
+      tmp_e.addr = client_addr;
+      command_list.push(tmp_e);
+    }
+    memset(buf, '\0', len); // 清空buf
+  }
+  close(client_sockfd);
+  printf("Disconnect client iP: %s, port: %d\n",
+         inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, ipbuf, sizeof(ipbuf)),
+         ntohs(client_addr.sin_port));
+  return;
+}
+void Coor::server() {
   // Prepare socket
   int server_sockfd = socket(PF_INET, SOCK_STREAM, 0);
   if (server_sockfd == -1) {
@@ -32,8 +70,56 @@ void Coor::init() {
     perror("bind error");
   }
 
-  if (listen(server_sockfd, participant.size()))
-    ;
+  if (listen(server_sockfd, 1024) == -1) {
+    close(server_sockfd);
+    perror("listen error");
+  }
+
+  printf("Listen on port %d\n", coordinator[0].port);
+
+  while (1) {
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+    int client_sockfd =
+        accept(server_sockfd, (struct sockaddr *)&client_addr, &client_len);
+
+    std::thread tcpThread(get_command, client_sockfd, client_addr);
+    tcpThread.detach(); // 不阻塞主线程，执行tcp通讯的线程从线程对象分离
+  }
+  close(server_sockfd);
+  return;
+}
+// FSM
+void Coor::FSM() {
+  std::thread pc_communit[participant.size()];
+  while (true) {
+    switch (curr_state) {
+    case FSM_state::init:
+      init();
+      break;
+    case FSM_state::wait_ready:
+      wait();
+      break;
+    case FSM_state::abort:
+      abort();
+      break;
+    case FSM_state::commit:
+      commit();
+      break;
+    default:
+      break;
+    }
+  }
+}
+void Coor::init() {
+  {
+    std::lock_guard<std::mutex> lockGuard(m);
+    curr_event = command_list.front();
+    command_list.pop();
+  }
+
+  return;
 }
 void Coor::wait() { return; }
 void Coor::abort() { return; }
--- Lab3/src/Node.hpp
+++ Lab3/src/Node.hpp
@@ -2,34 +2,56 @@
 #define NODE_HPP
 
 #include "include/kv.hpp"
+#include "include/parser.hpp"
 #include "queue"
 #include "read_conf.hpp"
 #include "thread"
 
+#include <arpa/inet.h>
+#include <cstring>
+#include <ctype.h>
+#include <mutex>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <condition_variable>
+
 using std::queue;
 
 namespace kv_2pc_sys {
+struct Cp_lock {
+  std::mutex m;
+  std::condition_variable empty;
+  int size;
+};
 
+enum FSM_state { init = 0, wait_ready, abort, commit };
 struct Event {
-  std::string command_list;
+  std::string command;
+  std::string reply;
   unsigned long long e_id;
-  string ip;
-  int port;
+  struct sockaddr_in addr;
 };
 
 class Node {
-private:
-  queue<std::string> command_list;
+protected:
+  queue<Event> command_list;
   KV kvdb;
+  FSM_state curr_state;
+  Event curr_event;
 
 public:
-  Node(){}
-  ~Node(){}
+  Node() {}
+  ~Node() {}
 
-  virtual void get_command(){}
-  virtual bool parse_command(){}
-  virtual void exe(){}
+  virtual void get_command(const int client_sockfd,
+                           const struct sockaddr_in &client_addr) {}
+  virtual void exe() {}
 
+  virtual void server(){}
+  virtual void work_main(){};
+  virtual void FSM() = 0;
   virtual void pc() = 0;
   virtual void init() = 0;
   virtual void wait() = 0;
@@ -40,12 +62,14 @@ public:
 class Coor : public Node {
 private:
 public:
-  Coor(){}
-  ~Coor(){}
+  Coor() {}
+  ~Coor() {}
 
-  virtual void get_command();
-  virtual bool parse_command();
-
+  virtual void get_command(const int client_sockfd,
+                           const struct sockaddr_in &client_addr);
+  virtual void server();
+  virtual void work_main();
+  virtual void FSM();
   virtual void init();
   virtual void wait();
   virtual void abort();
@@ -56,11 +80,12 @@ public:
 class Part : public Node {
 private:
 public:
-  Part(){}
-  ~Part(){}
+  Part() {}
+  ~Part() {}
 
   virtual void exe();
 
+  virtual void FSM();
   virtual void init();
   virtual void wait();
   virtual void abort();
